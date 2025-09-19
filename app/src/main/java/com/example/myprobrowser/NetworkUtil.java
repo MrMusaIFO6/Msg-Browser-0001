89de6b9843b17628772e3ecb71c8b8cf99a1bd57
MrMusaIFO6 <musa.mj609@gmail.com>
2025-09-19 07:15:06 +0600

    Update NetworkUtil.java

--- app/src/main/java/com/example/myprobrowser/NetworkUtil.java
+++ app/src/main/java/com/example/myprobrowser/NetworkUtil.java
@@ -1,1 +1,17 @@
+package com.example.myprobrowser;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtil {
+    public static boolean isConnected(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            NetworkInfo info = cm.getActiveNetworkInfo();
+            return info != null && info.isConnected();
+        }
+        return false;
+    }
+}
